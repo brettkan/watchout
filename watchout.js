12829c5ecdac579c2df7553ffd91cdc54066a0e8
Hack Reactor Students <accounts@hackreactor.com>
2015-05-22 17:22:52 -0700

    Added player circle to board

--- watchout.js
+++ watchout.js
@@ -38,6 +38,11 @@ var Enemy = function(leftOrRight) {
   this.moveFirst = leftOrRight;
 }
 
+var Player = function() {
+  this.x = width / 2;
+  this.y = height / 2
+}
+
 var allEnemies = function(num) {
   var enemies = [];
   for (var i = 0; i < num; i++) {
@@ -46,17 +51,17 @@ var allEnemies = function(num) {
   return enemies;
 };
 
-var createCircle = function(x, y) {
-  var circle = d3.select("svg").append("circle")
-    .attr("r",10)
-    .attr("cx",x)
-    .attr("cy",y)
-    .attr("opacity", 0)
-    .transition()
-    .duration(2000)
-    .attr("opacity", 1);
+// var createCircle = function(x, y) {
+//   var circle = d3.select("svg").append("circle")
+//     .attr("r",10)
+//     .attr("cx",x)
+//     .attr("cy",y)
+//     .attr("opacity", 0)
+//     .transition()
+//     .duration(2000)
+//     .attr("opacity", 1);
 
-}
+// }
 
 //function that generates all circles
 // var circleGod = function(n) {
@@ -73,25 +78,38 @@ var moveEnemies = function() {
 var enemies = allEnemies(10);
 
 
-d3.selectAll("circle")
-  .data(enemies);
+// d3.selectAll("circle")
+//   .data(enemies);
 
 var createBoard = function() {
-  svg.selectAll('circle').data(enemies)
+  svg.selectAll('circle.enemy')
+    .data(enemies)
     .enter()
     .append("circle")
     .attr("cx", function(d) { return d.x; })
     .attr("cy", function(d) { return d.y; })
     .attr("r", 10)
+    .attr("class", "enemy")
+    .attr("opacity", 0)
+    .transition()
+    .duration(2000)
+    .attr("opacity", 1);
+//debugger;
+  svg.selectAll('circle.player')
+    .data([new Player()])
+    .enter()
+    .append("circle")
+    .attr("cx", function(d) { return d.x; })
+    .attr("cy", function(d) { return d.y; })
+    .attr("r", 10)
+    .attr("fill","yellow")
+    .attr("class", "player")
     .attr("opacity", 0)
     .transition()
     .duration(2000)
     .attr("opacity", 1);
 }
 
-
-
-createBoard();
 
 //function
   //after waiting a defined interval of time
@@ -107,8 +125,13 @@ var moveEnemies = function() {
     .attr("cy", function(d) { return d.y; });
 }
 
-d3.timer(moveEnemies, 3000);
-//timeout
+createBoard();
+
+// d3.timer(moveEnemies, 3000);
+setInterval(moveEnemies, 3000);
+
+
+
 
 
 
